c35fd6abc4ecedace260c9534735fc431090d7bc
RashedElam <rashedelham3@gmail.com>
2024-08-18 06:28:41 +1000

    part12.6_numbers

--- part12-Part12_06.Numbers/src/main/java/Program.java
+++ part12-Part12_06.Numbers/src/main/java/Program.java
@@ -6,6 +6,16 @@ public class Program {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+        
+        System.out.println("How many random numbers should be printed?");
+        int number = Integer.valueOf(scanner.nextLine());
+        int i = 0;
+        while(i<number){
+            int rand = random.nextInt(11);
+            System.out.println(rand);
+            i++;
+        }
     }
 
 }
