1c37ef75ec2a2b46a88e7655c1d03eea13552feb
RashedElam <rashedelham3@gmail.com>
2024-08-20 08:54:50 +1000

    part13.06_Notifier

--- part13-Part13_06.Notifier/src/main/java/notifier/NotifierApplication.java
+++ part13-Part13_06.Notifier/src/main/java/notifier/NotifierApplication.java
@@ -1,11 +1,37 @@
 package notifier;
 
 
-public class NotifierApplication {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 
+
+public class NotifierApplication extends Application{
+
+    @Override
+    public void start(Stage window){
+        TextField text = new TextField("write here...");
+        Button button = new Button("Update");
+        Label label = new Label("");
+        
+        button.setOnAction((event)->{
+            label.setText(text.getText());
+        });
+        VBox vb = new VBox();
+        vb.getChildren().addAll(text,button,label);
+        
+        Scene scene = new Scene(vb);
+
+        window.setScene(scene);
+        window.show();
+    }
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        launch(NotifierApplication.class);
     }
 
 }
