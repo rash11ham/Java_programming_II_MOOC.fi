bf70ca861d097346fc49657563140f6a434b1c81
RashedElam <rashedelham3@gmail.com>
2024-08-20 14:51:47 +1000

    part13.08_UserTitle

--- part13-Part13_08.UserTitle/src/main/java/title/UserTitle.java
+++ part13-Part13_08.UserTitle/src/main/java/title/UserTitle.java
@@ -1,7 +1,21 @@
 package title;
 
+import java.util.Scanner;
+import javafx.application.Application;
+import javafx.stage.Stage;
 
-public class UserTitle {
 
+public class UserTitle extends Application{
 
+    
+    @Override
+    public void start(Stage window) {
+        
+        Parameters params = getParameters();
+        String name = params.getNamed().get("title");
+
+        window.setTitle(name);
+        window.show();
+    }
+    
 }
